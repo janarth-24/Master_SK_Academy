7f80f5b93cec21ad9233c335129819cc94f47812
janarthanan <janarthanan0012@gmail.com>
2025-08-13 20:28:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("enquire-btn").addEventListener("click", function()
+{
+    window.open("https://wa.me/918608200435", "_blank")
+});
+document.getElementById("contact-btn").addEventListener("click", function() {
+    window.location.href = "tel:+918608200435";
+});
